9bc2c769dc402686df0e6124486adeba7e7f1f3d
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-12 21:48:43 +0530

    changed the data structure

--- src/line.js
+++ src/line.js
@@ -1,20 +1,18 @@
 class Line {
-  constructor(x1, y1, x2, y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  constructor(endA, endB) {
+    this.endA = endA;
+    this.endB = endB;
   }
   get toString() {
-    let position1 = `(${this.x1}, ${this.y1})`;
-    let position2 = `(${this.x2}, ${(this, this.y2)})`;
+    let position1 = `(${this.endA.x}, ${this.endA.y})`;
+    let position2 = `(${this.endB.x}, ${this.endB.y})`;
     return `Line : ${position1} ${position2}`;
   }
   isEqual(other) {
-    let isX1Equal = this.x1 == other.x1;
-    let isY1Equal = this.y1 == other.y1;
-    let isX2Equal = this.x2 == other.x2;
-    let isY2Equal = this.y2 == other.y2;
+    let isX1Equal = this.endA.x == other.endA.x;
+    let isY1Equal = this.endA.y == other.endA.y;
+    let isX2Equal = this.endB.x == other.endB.x;
+    let isY2Equal = this.endB.y == other.endB.y;
     let isTypeEqual = this instanceof Line && other instanceof Line;
     return isX1Equal && isY1Equal && isX2Equal && isY2Equal && isTypeEqual;
   }
--- test/testLine.js
+++ test/testLine.js
@@ -4,21 +4,21 @@ const Line = require("../src/line");
 describe("testLine", function() {
   describe("isEqual", function() {
     it("should give true when the two instances are equal", function() {
-      let testLine = new Line(10, 11, 12, 13);
-      let otherLine = new Line(10, 11, 12, 13);
+      let testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
+      let otherLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       let actual = testLine.isEqual(otherLine);
       assert.ok(actual);
     });
     it("should give false when the two instances are not equal", function() {
-      let testLine = new Line(10, 11, 12, 13);
-      let otherLine = { x1: 10, y1: 11, x2: 12, y2: 13 };
+      let testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
+      let otherLine = { endA: { x: 10, y: 11 }, endB: { x: 12, y: 13 } };
       let actual = testLine.isEqual(otherLine);
       assert.ok(!actual);
     });
   });
   describe("testToString", function() {
     it("should give the string format of the object", function() {
-      let testLine = new Line(10, 11, 12, 13);
+      let testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       let actual = testLine.toString;
       let expected = "Line : (10, 11) (12, 13)";
       assert.strictEqual(actual, expected);
