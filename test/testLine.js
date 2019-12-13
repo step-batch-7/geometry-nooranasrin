65088a540b94cba76e3faf4526806ebd1eb492c7
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-13 11:47:55 +0530

    updated test

--- test/testLine.js
+++ test/testLine.js
@@ -4,30 +4,30 @@ const Line = require("../src/line");
 describe("testLine", function() {
   describe("isEqual", function() {
     it("should give true when the two instances are equal", function() {
-      let testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
-      let otherLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
-      let actual = testLine.isEqual(otherLine);
+      const testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
+      const otherLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
+      const actual = testLine.isEqual(otherLine);
       assert.ok(actual);
     });
     it("should give false when the two instances are not equal", function() {
-      let testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
-      let otherLine = { endA: { x: 10, y: 11 }, endB: { x: 12, y: 13 } };
-      let actual = testLine.isEqual(otherLine);
+      const testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
+      const otherLine = { endA: { x: 10, y: 11 }, endB: { x: 12, y: 13 } };
+      const actual = testLine.isEqual(otherLine);
       assert.ok(!actual);
     });
     it("should give false when one is an empty object ", function() {
-      let testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
-      let otherLine = {};
-      let actual = testLine.isEqual(otherLine);
+      const testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
+      const otherLine = {};
+      const actual = testLine.isEqual(otherLine);
       assert.ok(!actual);
     });
   });
 
   describe("testToString", function() {
     it("should give the string format of the object", function() {
-      let testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
-      let actual = testLine.toString;
-      let expected = "Line : (10, 11) (12, 13)";
+      const testLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
+      const actual = testLine.toString;
+      const expected = "Line : (10, 11) (12, 13)";
       assert.strictEqual(actual, expected);
     });
   });
