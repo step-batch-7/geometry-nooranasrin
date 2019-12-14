c4b3b57ee68d447b9708103162836d935b4746cb
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-14 10:35:45 +0530

    added one function to find the y intercept of a line
    isParallel method will invalidate the overlapping lines
    added test cases to isParallel method

--- src/line.js
+++ src/line.js
@@ -4,6 +4,10 @@ const arePointsEqual = function(point1, point2) {
   return point1.x === point2.x && point1.y === point2.y;
 };
 
+const getYIntercept = function(x, y, m) {
+  return y - m * x;
+};
+
 class Line {
   constructor(endA, endB) {
     this.endA = { x: endA.x, y: endA.y };
@@ -33,7 +37,13 @@ class Line {
 
   isParallelTo(other) {
     if (!(other instanceof Line)) return false;
-    return this.slope === other.slope;
+    const thisYIntercept = getYIntercept(this.endA.x, this.endA.y, this.slope);
+    const otherYIntercept = getYIntercept(
+      other.endA.x,
+      other.endA.y,
+      other.slope
+    );
+    return thisYIntercept != otherYIntercept && this.slope === other.slope;
   }
 }
 
--- test/testLine.js
+++ test/testLine.js
@@ -2,24 +2,24 @@
 const assert = require("chai").assert;
 const Line = require("../src/line");
 
-describe("Line", function() {
-  describe("isEqual", function() {
-    it("should validate when the two instances are equal", function() {
+describe("Line", () => {
+  describe("isEqual", () => {
+    it("should validate when the two instances are equal", () => {
       const line = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       const otherLine = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       assert.isTrue(line.isEqual(otherLine));
     });
-    it("should invalidate when the two instances not equal", function() {
+    it("should invalidate when the two instances not equal", () => {
       const line = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       const otherLine = new Line({ x: 11, y: 11 }, { x: 12, y: 13 });
       assert.isFalse(line.isEqual(otherLine));
     });
-    it("should invalidate when one line is not an instance of Line class", function() {
+    it("should invalidate when one line is not an instance of Line class", () => {
       const line = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       const otherLine = { endA: { x: 10, y: 11 }, endB: { x: 12, y: 13 } };
       assert.isFalse(line.isEqual(otherLine));
     });
-    it("should invalidate when one is an empty object ", function() {
+    it("should invalidate when one is an empty object ", () => {
       const line = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       const otherLine = {};
       assert.isFalse(line.isEqual(otherLine));
@@ -30,79 +30,88 @@ describe("Line", function() {
     });
   });
 
-  describe("toString", function() {
-    it("should give the string format of the object", function() {
+  describe("toString", () => {
+    it("should give the string format of the object", () => {
       const line = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       assert.strictEqual(line.toString(), "Line : (10, 11) (12, 13)");
     });
   });
 
-  describe("length", function() {
-    it("should give the length when the coordinates are positive", function() {
+  describe("length", () => {
+    it("should give the length when the coordinates are positive", () => {
       const line = new Line({ x: 2, y: 1 }, { x: 6, y: 4 });
       assert.deepStrictEqual(line.length, 5);
     });
-    it("should give the length of the given line when one of the coordinate is negative", function() {
+    it("should give the length of the given line when one of the coordinate is negative", () => {
       const line = new Line({ x: -2, y: 3 }, { x: 5, y: 2 });
       assert.approximately(line.length, 7.07, 7.07);
     });
-    it("should give the length of the given line when the coordinates are negative", function() {
+    it("should give the length of the given line when the coordinates are negative", () => {
       const line = new Line({ x: -2, y: -3 }, { x: -3, y: -5 });
       assert.approximately(line.length, 2.236, 2.2);
     });
-    it("should calculate length when line having same end points", function() {
+    it("should calculate length when line having same end points", () => {
       const line = new Line({ x: 1, y: 2 }, { x: 1, y: 2 });
       assert.strictEqual(line.length, 0);
     });
   });
 
-  describe("isParallelTo", function() {
-    it("should validate when two lines are parallel", function() {
+  describe("isParallelTo", () => {
+    it("should validate when two lines are parallel", () => {
       const line = new Line({ x: 3, y: 4 }, { x: 7, y: 5 });
       const otherLine = new Line({ x: 5, y: 5 }, { x: 9, y: 6 });
       assert.isTrue(line.isParallelTo(otherLine));
     });
-    it("should validate when one line is not an instance of class", function() {
+    it("should validate when one line is not an instance of class", () => {
       const line = new Line({ x: 3, y: 4 }, { x: 7, y: 5 });
       const otherLine = { endA: { x: 5, y: 5 }, endB: { x: 9, y: 6 } };
       assert.isFalse(line.isParallelTo(otherLine));
     });
-    it("should invalidate when two lines are not parallel", function() {
+    it("should invalidate when two lines are not parallel", () => {
       const line = new Line({ x: 3, y: 5 }, { x: 7, y: 5 });
       const otherLine = new Line({ x: 5, y: 5 }, { x: 9, y: 6 });
       assert.isFalse(line.isParallelTo(otherLine));
     });
-    it("should invalidate when one is an empty object ", function() {
+    it("should invalidate when one is an empty object ", () => {
       const line = new Line({ x: 10, y: 11 }, { x: 12, y: 13 });
       const otherLine = {};
       assert.isFalse(line.isParallelTo(otherLine));
     });
-    it("should invalidate if lines are perpendicular to each other", function() {
+    it("should invalidate if lines are perpendicular to each other", () => {
       let line1 = new Line({ x: -3, y: -2 }, { x: 1, y: -2 });
       let line2 = new Line({ x: 3, y: 2 }, { x: -1, y: -2 });
       assert.isFalse(line1.isParallelTo(line2));
     });
-    it("should invalidate if lines intersect each other", function() {
+    it("should invalidate if lines intersect each other", () => {
       let line1 = new Line({ x: 3, y: 2 }, { x: 1, y: 3 });
       let line2 = new Line({ x: 3.2, y: 2 }, { x: -1, y: 1 });
       assert.isFalse(line1.isParallelTo(line2));
     });
+    it("should invalidate overlapping lines", () => {
+      let line1 = new Line({ x: 0, y: 0 }, { x: 1, y: 1 });
+      let line2 = new Line({ x: 2, y: 2 }, { x: 3, y: 3 });
+      assert.isFalse(line1.isParallelTo(line2));
+    });
+    it("should invalidate overlapping lines", () => {
+      let line1 = new Line({ x: 0, y: 0 }, { x: 1, y: 1 });
+      assert.isFalse(line1.isParallelTo(line1));
+    });
   });
 
-  describe("slope", function() {
-    it("should give the slope of a line when slope is positive", function() {
+  describe("slope", () => {
+    it("should give the slope of a line when slope is positive", () => {
       const line = new Line({ x: 3, y: 4 }, { x: 7, y: 5 });
       assert.approximately(line.slope, 0.25, 0.25);
     });
-    it("should give 0 as slope when the line is parallel to x axis", function() {
+    it("should give 0 as slope when the line is parallel to x axis", () => {
       const line = new Line({ x: -3, y: 4 }, { x: 3, y: 4 });
       assert.approximately(line.slope, 0, 0);
     });
-    it("should give the infinity when the line is parallel to y axis", function() {
+    it("should give the infinity when the line is parallel to y axis", () => {
       const line = new Line({ x: 4, y: -4 }, { x: 4, y: 4 });
       assert.strictEqual(line.slope, Infinity);
     });
-    it("Should give slope of a line when slope is negative", function() {
+    it("Should give slope of a line when slope is negative", () => {
       const line = new Line({ x: 7, y: 2 }, { x: 3, y: 3 });
       assert.strictEqual(line.slope, -0.25);
     });
