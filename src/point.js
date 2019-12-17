9130232236de99b5488510ba6891eebf92956de0
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-17 07:30:45 +0530

    updated isEqualTo method of class Point

--- src/point.js
+++ src/point.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const arePointsEqual = function(point1, point2) {
-  return point1.x === point2.x && point1.y === point2.y;
-};
-
 class Point {
   constructor(x, y) {
     [this.x, this.y] = [x, y];
@@ -11,7 +7,7 @@ class Point {
 
   isEqualTo(other) {
     if (!(other instanceof Point)) return false;
-    return arePointsEqual(this, other);
+    return this.x === other.x && this.y === other.y;
   }
   toString() {
     return `[Point @(${this.x},${this.y})]`;
